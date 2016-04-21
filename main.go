deeb5b6f190ff429cf9b7327f60c7cb16e3c6165
James Findley <jamesf@nutmeg.com>
2016-04-21 09:16:17 +0100

    Add support for nutcracker certs

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"errors"
 	"flag"
 	"html/template"
 	"io/ioutil"
@@ -19,6 +27,7 @@ var (
 	csrfWrapper        func(http.Handler) http.Handler
 	nutcrackerServer   string
 	nutcrackerKey      string
+	nutcrackerCertName string
 	listen             string
 	ssl                bool
 	tmpl               *template.Template
@@ -33,6 +42,7 @@ var (
 func init() {
 	flag.StringVar(&nutcrackerServer, "backend", "localhost:8443", "Nutcracker Backend")
 	flag.StringVar(&nutcrackerKey, "key", "", "Nutcracker key")
+	flag.StringVar(&nutcrackerCertName, "cert", "", "Name of TLS cert in nutcracker")
 	flag.BoolVar(&ssl, "secure", true, "Use this to disable TLS.  Do not use in production!")
 	flag.StringVar(&listen, "listen", "0.0.0.0:8443", "Listen address")
 	flag.Parse()
@@ -69,7 +79,94 @@ func addRoutes(r *mux.Router) {
 	r.HandleFunc("/admin", Admin).Methods("GET", "POST").Name("Admin")
 }
 
-func server() {
+func getNutcrackerCert(api *nutcracker.API) (cert tls.Certificate, err error) {
+	buf, err := api.Post("/secrets/view", nutcracker.NewAPIReq().Set("name", nutcrackerCertName))
+	if err != nil {
+		return
+	}
+	decoded := make([]byte, base64.StdEncoding.DecodedLen(len(buf)-8))
+	n, err := base64.StdEncoding.Decode(decoded, buf[8:])
+
+	decoded = decoded[:n]
+
+	var certList [][]byte
+
+	for len(decoded) > 0 {
+		var block *pem.Block
+		block, decoded = pem.Decode(decoded)
+		if block == nil {
+			break
+		}
+
+		if block.Type == "CERTIFICATE" {
+			// letsencrypt returns the bundle in the wrong order.
+			// make a list and reverse it.
+			certList = append(certList, block.Bytes)
+
+		}
+
+		if block.Type == "PRIVATE KEY" {
+
+			cert.PrivateKey, err = parsePrivateKey(block.Bytes)
+			if err != nil {
+				continue
+			}
+
+		}
+
+		// Reverse the list
+		for i := len(certList) - 1; i >= 0; i-- {
+			cert.Certificate = append(cert.Certificate, certList[i])
+		}
+
+	}
+
+	return
+
+}
+
+// Tanke from crypto/x509
+func parsePrivateKey(der []byte) (crypto.PrivateKey, error) {
+	if key, err := x509.ParsePKCS1PrivateKey(der); err == nil {
+		return key, nil
+	}
+	if key, err := x509.ParsePKCS8PrivateKey(der); err == nil {
+		switch key := key.(type) {
+		case *rsa.PrivateKey, *ecdsa.PrivateKey:
+			return key, nil
+		default:
+			return nil, errors.New("crypto/tls: found unknown private key type in PKCS#8 wrapping")
+		}
+	}
+	if key, err := x509.ParseECPrivateKey(der); err == nil {
+		return key, nil
+	}
+
+	return nil, errors.New("crypto/tls: failed to parse private key")
+}
+
+func main() {
+
+	serverCreds := &nutcracker.Creds{
+		Username: nutcrackerUser,
+		Password: nutcrackerKey,
+	}
+
+	api := nutcracker.NewAPI(
+		serverCreds,
+		nutcrackerServer)
+
+	resp, err := api.Post(
+		"/secrets/view",
+		nutcracker.NewAPIReq().Set(
+			"name",
+			nutcrackerCSRFName))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	csrfWrapper = csrf.Protect(resp, csrf.Secure(ssl))
+
 	// Start metric fetcher
 	go updateMetrics()
 
@@ -84,7 +181,19 @@ func server() {
 
 	if ssl {
 
-		cert, err := GenCert()
+		var cert tls.Certificate
+		var err error
+
+		if nutcrackerCertName == "" {
+			// Use a self-signed cert
+			cert, err = GenCert()
+		} else {
+			cert, err = getNutcrackerCert(api)
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		sock, err := Socket(listen, cert)
 		if err != nil {
 			log.Fatal(err)
@@ -96,28 +205,4 @@ func server() {
 		server.Addr = listen
 		server.ListenAndServe()
 	}
-
-}
-
-func main() {
-
-	serverCreds := &nutcracker.Creds{
-		Username: nutcrackerUser,
-		Password: nutcrackerKey,
-	}
-
-	resp, err := nutcracker.NewAPI(
-		serverCreds,
-		nutcrackerServer).Post(
-		"/secrets/view",
-		nutcracker.NewAPIReq().Set(
-			"name",
-			nutcrackerCSRFName))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	csrfWrapper = csrf.Protect(resp, csrf.Secure(ssl))
-
-	server()
 }
